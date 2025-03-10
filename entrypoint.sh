d96bb499929122b92b1a1a1c4f433224c0194130
Wassim Tout <>
2025-03-10 17:35:40 +0000

    Moved NEW_TAG assignment inside the if statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,19 @@
 #! /bin/bash -l
 
 CURR_VER="${1}"
-PREFIX=$(echo ${CURR_VER} | grep -E '^[a-zA-Z]' &>/dev/null && echo "${CURR_VER}" | tr '[0-9]' ' ' | awk -F ' ' '{print $1}' || echo '	')
-VERSION=$(echo "${CURR_VER}" | awk -F "${PREFIX}" '{print $NF}')
-VERLEN=$(echo ${VERSION//./ } | wc -w)
-VERARR=(${VERSION//./ })
 
 if [[ "${CURR_VER}" == "" ]]
 then
 	NEW_TAG="0.0.1"
 else
+	PREFIX=$(echo ${CURR_VER} | grep -E '^[a-zA-Z]' &>/dev/null && echo "${CURR_VER}" | tr '[0-9]' ' ' | awk -F ' ' '{print $1}' || echo '	')
+	VERSION=$(echo "${CURR_VER}" | awk -F "${PREFIX}" '{print $NF}')
+	VERLEN=$(echo ${VERSION//./ } | wc -w)
+	VERARR=(${VERSION//./ })
 	for i in ${!VERARR[@]}
 	do
 		export P$((i + 1))="${VERARR[i]}"
 	done
-
 	if [[ "${P3}" -lt 20 ]]
 	then
 		[[ "${P3}" != "" ]] && P3=$((P3 + 1))
@@ -27,12 +26,11 @@ else
 		[[ "${P2}" != "" ]] && P2=0
 		P1=$((P1 + 1))
 	fi
-
 	for i in ${!VERARR[@]}
 	do
 		declare -n tmp="P$((i + 1))"
 		VERARR[i]="${tmp}"
 	done
-fi
 	NEW_TAG=$(echo ${PREFIX}${VERARR[@]} | tr ' ' '.')
+fi
 echo "next-tag=${NEW_TAG}" >> $GITHUB_OUTPUT
