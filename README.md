8335b825eb8c81a4e4d4621e290029ec79fc4da5
Wassim Tout <>
2025-03-10 16:47:14 +0000

    Initial action commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Get next tag action
+
+This action returns the bumped up tag
+
+## Intputs
+
+## `current-tag`
+
+The tag found in the repository. Default value is ''
+
+## Outputs
+
+## `next-tag`
+
+The bumped up input tag. If the input is '' or not provided, the next tag will be '0.0.1'
+
+## Example usage
+
+uses: wtout/get-next-tag-action@v1
---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#! /bin/bash -l
+
+CURR_VER="${1}"
+PREFIX=$(echo "${CURR_VER}" | tr '[0-9]' ' ' | awk -F ' ' '{print $1}')
+VERSION=$(echo "${CURR_VER}" | awk -F "${PREFIX}" '{print $NF}')
+VERLEN=$(echo ${VERSION//./ } | wc -w)
+VERARR=(${VERSION//./ })
+
+if [[ "${CURR_VER}" == "" ]]
+then
+	NEW_TAG="0.0.1"
+else
+	for i in ${!VERARR[@]}
+	do
+		export P$((i + 1))="${VERARR[i]}"
+	done
+
+	if [[ "${P3}" -lt 20 ]]
+	then
+		[[ "${P3}" != "" ]] && P3=$((P3 + 1))
+	elif [[ "${P2}" -lt 20 ]]
+	then
+		[[ "${P3}" != "" ]] && P3=0
+		[[ "${P2}" != "" ]] && P2=$((P2 + 1))
+	else
+		[[ "${P3}" != "" ]] && P3=0
+		[[ "${P2}" != "" ]] && P2=0
+		P1=$((P1 + 1))
+	fi
+
+	for i in ${!VERARR[@]}
+	do
+		declare -n tmp="P$((i + 1))"
+		VERARR[i]="${tmp}"
+	done
+fi
+	NEW_TAG=$(echo ${PREFIX}${VERARR[@]} | tr ' ' '.')
+echo "next-tag=${NEW_TAG}" >> $GITHUB_OUTPUT
