f718147ca0fe8959667a6882e335040b3fc5bba1
Wassim Tout <>
2025-03-20 22:28:38 +0000

    Modified the action to properly bump the version depending on the release type

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ This action returns the bumped up tag
 
 ## Intputs
 
+## `release-type`
+
+**Required** The release type can be: Major, Minor, or HotFix
+
 ## `current-tag`
 
 The tag found in the repository. Default value is ''
@@ -12,8 +16,15 @@ The tag found in the repository. Default value is ''
 
 ## `next-tag`
 
-The bumped up input tag. If the input is '' or not provided, the next tag will be '0.0.1'
+The bumped up input tag.
+If the current-tag is '' or omitted, the next tag will be '`1.0.0`' regardless of the release-type value.
+If the release-type is 'Major' and the current-tag is '2', the next-tag will be '`3`'.
+If the release-type is 'Minor' and the current-tag is 'v2', the next-tag will be '`v2.1`'.
+If the release-type is 'HotFix' and the current-tag is 'v2', the next-tag will be '`v2.0.1`'.
 
 ## Example usage
 
-uses: wtout/get-next-tag-action@v4
+uses: wtout/get-next-tag-action@v5
+with:
+	release-type: Major
+	current-tag: 1.0.0
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,11 @@
 #! /bin/bash -l
 
-CURR_VER="${1}"
+REL_TYPE="${1}"
+CURR_VER="${2}"
 
-if [[ "${CURR_VER}" == "" ]]
+if [[ ${CURR_VER} == "" ]]
 then
-	NEW_TAG="0.0.1"
+	NEW_TAG="1.0.0"
 else
 	PREFIX=$(echo ${CURR_VER} | grep -E '^[a-zA-Z]' &>/dev/null && echo "${CURR_VER}" | tr '[0-9]' ' ' | awk -F ' ' '{print $1}' || echo '	')
 	VERSION=$(echo "${CURR_VER}" | awk -F "${PREFIX}" '{print $NF}')
@@ -14,19 +15,30 @@ else
 	do
 		export P$((i + 1))="${VERARR[i]}"
 	done
-	if [[ "${P3}" -lt 20 ]]
-	then
-		[[ "${P3}" != "" ]] && P3=$((P3 + 1))
-	elif [[ "${P2}" -lt 20 ]]
-	then
-		[[ "${P3}" != "" ]] && P3=0
-		[[ "${P2}" != "" ]] && P2=$((P2 + 1))
-	else
-		[[ "${P3}" != "" ]] && P3=0
-		[[ "${P2}" != "" ]] && P2=0
-		P1=$((P1 + 1))
-	fi
-	for i in ${!VERARR[@]}
+	case ${REL_TYPE,,} in
+		hotfix)
+			P3=${P3:=0}
+			P3=$((P3 + 1))
+			[[ ${P2} == "" ]] && P2=0
+			VERLEN=3
+			;;
+		minor)
+			{ [[ ${P3} != "" ]] && P3=0; } || VERLEN=2
+			P2=${P2:=0}
+			P2=$((P2 + 1))
+			;;
+		major)
+			{ [[ ${P3} != "" ]] && P3=0; } || VERLEN=2
+			{ [[ ${P2} != "" ]] && P2=0; } || VERLEN=1
+			P1=${P1:=0}
+			P1=$((P1 + 1))
+			;;
+		*)
+			echo "Valid values for the release_type: Major, Minor, or HotFix"
+			exit 1
+			;;
+	esac
+	for i in $(seq 0 $((VERLEN - 1)))
 	do
 		declare -n tmp="P$((i + 1))"
 		VERARR[i]="${tmp}"
